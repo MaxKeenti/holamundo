e59540d8743c235e6f78dc4e09dc9c1b96edd0cd
Max Gonzalez <52800035+MaxKeenti@users.noreply.github.com>
2026-03-13 10:47:52 -0600

    feat: Add Interface components to greet what's placed on textfield

--- app/src/main/java/com/maxkeenti/holamundo/MainActivity.kt
+++ app/src/main/java/com/maxkeenti/holamundo/MainActivity.kt
@@ -1,47 +1,50 @@
 package com.maxkeenti.holamundo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.maxkeenti.holamundo.ui.theme.HolaMundoTheme
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            HolaMundoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            InterfaceCustom()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun InterfaceCustom() {
+    var name by rememberSaveable{mutableStateOf("")}
+
+    Text(text = "Welcome to my app")
+    TextField(
+        value = name,
+        onValueChange = { name = it },
+        label = { Text(text = "What's your name?") },
     )
+
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Hello $name!", Toast.LENGTH_SHORT).show()
+    }){
+        Text(text = "Greet")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HolaMundoTheme {
-        Greeting("Android")
-    }
+    InterfaceCustom()
 }
